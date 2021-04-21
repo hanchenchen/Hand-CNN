ca2a58af6db5bce3c217c9db8d13fa23364733cb
root <13980209828@163.com>
2021-04-21 16:37:54 +0800

    coco+TV

--- datasets/split_coco.py
+++ datasets/split_coco.py
@@ -1,6 +1,6 @@
 import random
 random.seed(1)
-dataset_path = 'COCO-Hand-S'
+
 def split_train_val_test(dataset, anno_list):
     anno_list = ['datasets/' + dataset + '/' + dataset + '_images/' + i for i in anno_list]
     random.shuffle(anno_list)
@@ -16,8 +16,48 @@ def split_train_val_test(dataset, anno_list):
     test_dev.close()
 
 
-origin_anno = open(dataset_path + '/COCO-Hand-S_annotations.txt', 'r').read()
-origin_anno = origin_anno.split('\n')
-print(origin_anno[:10], len(origin_anno))
+dataset = {'COCO-S': {'anno_train': './datasets/COCO-Hand/COCO-Hand-S/COCO-Hand-S_annotations.txt',
+                      'imgs': './datasets/COCO-Hand/COCO-Hand-S/COCO-Hand-S_Images/',
+                      'prefix': '../COCO-Hand/COCO-Hand-S/COCO-Hand-S_Images/'},
+           'COCO-Big': {'anno_train': './datasets/COCO-Hand/COCO-Hand-Big/COCO-Hand-Big_annotations.txt',
+                        'imgs': './datasets/COCO-Hand/COCO-Hand-Big/COCO-Hand-Big_Images/',
+                        'prefix': '../COCO-Hand/COCO-Hand-S/COCO-Hand-S_Images/'},
+           'TV': {'anno_train': './datasets/TV-Hand/train_IDs_tv.txt',
+                  'anno_test': './datasets/TV-Hand/test_IDs.txt',
+                  'anno_valid': './datasets/TV-Hand/valid_IDs.txt',
+                  'imgs': './datasets/TV-Hand/TV-Hand_Images',
+                  'prefix': '../TV-Hand/TV-Hand_Images/'}}
 
-split_train_val_test(dataset_path, origin_anno)
+
+def get_list(name, c = '_train'):
+    l = open(dataset[name]['anno'+c], 'r').read().split('\n')
+    # if l[0].split(',').split('/')
+    l = [dataset[name]['prefix'] + i.split(',')[0].split('/')[-1] for i in l if i]
+    print(l[:10])
+    return l
+
+
+
+train_list = get_list('COCO-S') + get_list('TV')
+val_list = get_list('TV', '_valid')
+test_dev_list = get_list('TV', '_test')
+
+
+random.shuffle(train_list)
+
+train = open('./datasets/train_annotations.txt', 'w')
+val = open('./datasets/val_annotations.txt', 'w')
+test_dev = open('./datasets/test_annotations.txt', 'w')
+train.write('\n'.join(train_list))
+val.write('\n'.join(val_list))
+test_dev.write('\n'.join(test_dev_list))
+train.close()
+val.close()
+test_dev.close()
+
+print(len(train_list))
+print(len(val_list))
+print(len(test_dev_list))
+# 14930
+# 1362
+# 4673
