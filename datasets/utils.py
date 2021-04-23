c201f963beee4402d5e1b08e99f094f49b0e0f5b
unknown <13980209828@163.com>
2021-04-23 14:40:35 +0800

    add inference time

---
+++ datasets/utils.py
@@ -0,0 +1,60 @@
+import json
+from PIL import Image, ImageDraw
+
+
+def draw_point(points, im):
+    i = 0
+    draw = ImageDraw.Draw(im)
+
+    for point in points:
+        x = point[0]
+        y = point[1]
+
+        if i == 0:
+            rootx = x
+            rooty = y
+        if i == 1 or i == 5 or i == 9 or i == 13 or i == 17:
+            prex = rootx
+            prey = rooty
+
+        if i > 0 and i <= 4:
+            draw.line((prex, prey, x, y), 'red')
+            draw.ellipse((x - 3, y - 3, x + 3, y + 3), 'red', 'white')
+        if i > 4 and i <= 8:
+            draw.line((prex, prey, x, y), 'yellow')
+            draw.ellipse((x - 3, y - 3, x + 3, y + 3), 'yellow', 'white')
+
+        if i > 8 and i <= 12:
+            draw.line((prex, prey, x, y), 'green')
+            draw.ellipse((x - 3, y - 3, x + 3, y + 3), 'green', 'white')
+        if i > 12 and i <= 16:
+            draw.line((prex, prey, x, y), 'blue')
+            draw.ellipse((x - 3, y - 3, x + 3, y + 3), 'blue', 'white')
+        if i > 16 and i <= 20:
+            draw.line((prex, prey, x, y), 'purple')
+            draw.ellipse((x - 3, y - 3, x + 3, y + 3), 'purple', 'white')
+
+        prex = x
+        prey = y
+        i = i + 1
+    return im
+
+
+def show_hand(im, label, save_path=None):
+    if type(im) == str:
+        im = Image.open(im)
+    im = draw_point(label, im)
+    if save_path:
+        print('save output to ', save_path)
+        im.save(save_path)
+    im.show()
+    return im
+
+def show_bbox(im, x_min, x_max, y_min, y_max, x1, y1, x2, y2, x3, y3, y4):
+    pass
+
+
+if __name__=="__main__":
+    im = '../00000.jpg'
+    label = json.load(open('../Point_2d.json', 'r'))["inroom2/001/001"]["00000.jpg"]
+    show_hand(im, label, './00000.jpg')
